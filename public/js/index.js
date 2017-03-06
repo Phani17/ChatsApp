a9d4a910035573c3b78925d9d15e682c6299de1c
Phanindra <phani1716@gmail.com>
2017-03-06 06:59:46 +0530

    Add new message and create evnets

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket=io();//initiaing request from client to server
+socket.on('connect',function(){
+  console.log('Connected to server');
+  // socket.emit('createEmail',{
+  //   to:'mahi@ex.com',
+  //   text:'U r gr8'
+  // });
+
+  socket.emit('createMessage',{
+    from:'Andrew',
+    text:'Hi how r u'
+  });
+});
+socket.on('disconnect',function(){
+  console.log('Disconnected from server');
+});
+// socket.on('newEmail',function (email) { //socket.on lets us setup event handlers for the individual socket. This could be a custom event like "newMessage" or a built-in one like "disconnect"
+//   console.log('New Email',email);
+// });
+socket.on('newMessage',function (message) { //socket.on lets us setup event handlers for the individual socket. This could be a custom event like "newMessage" or a built-in one like "disconnect"
+  console.log('New Message',message);
+});
--- server/server.js
+++ server/server.js
@@ -8,11 +8,28 @@ var app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
 app.use(express.static(publicPath));
-io.on('connection',(socket)=>{
-console.log('New user connected');
-socket.on('disconnect',()=>{
-  console.log('User was DisConnected');
-});
+
+io.on('connection',(socket)=>{  //allows to register event listener ,socket represnts individual socket not all users
+  console.log('New user connected');//io.on lets us listen for server-wide events like new socket connections. The callback function we create is where we can setup events for that newly connected socket
+  // socket.emit('newEmail',{
+  //   from:'phani@ex.com',
+  //   text:'Hey. How are u',
+  //   createAt:123
+  // });
+  socket.emit('newMessage',{
+    from:'john@ex.com',
+    text:'C u den',
+    createAt:456
+  });
+  socket.on('createMessage',function(message){
+    console.log('createMessage',message);
+  });
+  // socket.on('createEmail',(newEmail)=>{
+  //   console.log('createEmail',newEmail);
+  // });
+  socket.on('disconnect',()=>{
+    console.log('User was DisConnected');
+  });
 });
 server.listen(port,()=>{
   console.log(`Server is up on ${port}`);
